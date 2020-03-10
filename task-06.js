0f01564a2b7d9a9809259a7d7d1a2115cd15bc8a
Ghore <ghoreua@gmail.com>
2020-03-10 11:21:38 +0200

    homework-03

---
+++ task-06.js
@@ -0,0 +1,22 @@
+"use strict";
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const calculateTotalPrice = function(allProdcuts, productName) {
+  let productSum = 0;
+  for (const obj of allProdcuts) {
+    if (obj["name"] == productName) {
+      productSum += obj["price"] * obj["quantity"];
+      break;
+    } else continue;
+  }
+  return productSum;
+};
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
