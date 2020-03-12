7687b2ce993abbfd7ee801b78b6bc97c75ce3e0b
Ghore <ghoreua@gmail.com>
2020-03-12 14:31:49 +0200

    homework-03

--- task-03.js
+++ task-03.js
@@ -8,24 +8,27 @@ const findBestEmployee = function(employees) {
       max = employees[key];
       name = key;
     }
-    return name;
-  }
+}
+return name;
 };
-console.log(findBestEmployee({
+console.log(
+  findBestEmployee({
     ann: 29,
     david: 35,
     helen: 1,
     lorence: 99
   })
 ); // lorence
-console.log(findBestEmployee({
+console.log(
+  findBestEmployee({
     poly: 12,
     mango: 17,
     ajax: 4
   })
 ); // mango
 
-console.log(findBestEmployee({
+console.log(
+  findBestEmployee({
     lux: 147,
     david: 21,
     kiwi: 19,
